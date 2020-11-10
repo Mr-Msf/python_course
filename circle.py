14a044c20cb0496c24171e022875cc2d58cc89b5
MrMsf <mikaelsharifyfunk@gmail.com>
2020-11-10 18:49:34 +0000

    Added Comment

--- circle.py
+++ circle.py
@@ -4,4 +4,14 @@ def circumference(r):
   return 2*math.pi*r
 
 def area(r):
+  """Calculates the area given the radius
+  
+  Parameters:
+  ----------
+  r
+
+  Returns:
+  ----------
+  area
+  """
   return math.pi*r**2
